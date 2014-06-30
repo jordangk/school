982229eb0f649a4b5b965438812e1ad958760fd5
Jordan <jordan@wishpond.com>
2014-06-29 17:38:26 -0700

    adding commited files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+School::Application.config.secret_key_base = '63250d2088678457c4580654dca9161fede23aa974c7e084c0a8cd026f8e413179f7ba7fa7612e1089a13f8b7886c214775b510232f5e6512762b8b50616b59f'
